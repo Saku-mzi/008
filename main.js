bb4a04c1dc9ebd69b55a5e66ca6576a32b2c9a1e
Saku-mzi <sakumzisipamla@gmail.com>
2026-06-25 10:21:10 +0200

    Refactor main.js to streamline dark and light mode button functionality

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("ons is geland");
+  const darkButton = document.querySelector("button#dark");
+
+  //console.log("ons is geland", darkButton);
+
+  darkButton.addEventListener("click", () => {
+    document.querySelector("body").classList.add("dark-mode");
+
+    // console.log("dark bottom is gepressed");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lightButton = document.querySelector("button#lite");
+
+  //console.log("ons is geland", darkButton);
+
+  lightButton.addEventListener("click", () => {
+    document.querySelector("body").classList.remove("dark-mode");
+
+    // console.log("dark bottom is gepressed");
+  });
 });
